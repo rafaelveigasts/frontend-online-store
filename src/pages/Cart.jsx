74032a9bb0631c757f7ea3420acc60bc8857b56e
Matheus Laurindo <matheus104521@gmail.com>
2021-09-20 18:39:12 -0300

    Adiciona link para voltar a pagina inicial em carrinho

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Cart extends Component {
   render() {
     return (
       <section className="shopping-cart-page">
+        <Link to="/">
+          Voltar
+        </Link>
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
       </section>
     );
