817a95012ae6324ca7466707600957f4b4850af3
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-09-21 12:05:40 -0300

    Refatora componente ProductNotFound como funcional

--- src/Components/ProductNotFound/ProductNotFound.jsx
+++ src/Components/ProductNotFound/ProductNotFound.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-class ProductNotFound extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>Nenhum produto foi encontrado</p>
-      </div>
-    );
-  }
+export default function ProductNotFound() {
+  return (
+    <div>
+      <p>Nenhum produto foi encontrado</p>
+    </div>
+  );
 }
-
-export default ProductNotFound;
