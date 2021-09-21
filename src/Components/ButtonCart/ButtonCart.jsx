131967f69713d9ea8ab3d694d6269b9cdda01a59
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-09-21 12:04:09 -0300

    Refatora ButtonCart como componente funcional

--- src/Components/ButtonCart/ButtonCart.jsx
+++ src/Components/ButtonCart/ButtonCart.jsx
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class ButtonCart extends Component {
-  render() {
-    return (
-      <div className="btn-cart">
-        <Link to="/Cart">
-          <button type="button" data-testid="shopping-cart-button">
-            Carrinho
-          </button>
-        </Link>
-      </div>
-    );
-  }
+export default function ButtonCart() {
+  return (
+    <div className="btn-cart">
+      <Link to="/cart">
+        <button type="button" data-testid="shopping-cart-button">
+          Carrinho
+        </button>
+      </Link>
+    </div>
+  );
 }
-
-export default ButtonCart;
